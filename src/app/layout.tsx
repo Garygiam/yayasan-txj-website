3d83af11db3551a331042760b93fa11422ad6212
Gary Giam <garygiam@Garys-MacBook-Pro.local>
2026-06-20 17:25:01 +0800

    feat(analytics): install Google Analytics 4

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type {Metadata} from 'next'
+import {GoogleAnalytics} from '@next/third-parties/google'
 import {headers} from 'next/headers'
 import './globals.css'
 import {fontBody, fontDisplay, fontZhBody, fontZhDisplay} from './fonts'
@@ -25,13 +26,17 @@ export default async function RootLayout({
 }>) {
   const requestHeaders = await headers()
   const documentLanguage = resolveDocumentLanguage(requestHeaders.get('X-NEXT-INTL-LOCALE'))
+  const shouldLoadGoogleAnalytics = process.env.NODE_ENV === 'production'
 
   return (
     <html
       lang={documentLanguage}
       className={`${fontBody.variable} ${fontDisplay.variable} ${fontZhBody.variable} ${fontZhDisplay.variable}`}
     >
-      <body>{children}</body>
+      <body>
+        {children}
+        {shouldLoadGoogleAnalytics ? <GoogleAnalytics gaId="G-83JXN5MCR7" /> : null}
+      </body>
     </html>
   )
 }
